1791e535880e1fda0a0d0fe544713c71c6efa92a
Justin Armstrong <jxa1762@rit.edu>
2019-10-27 23:46:29 -0400

    DomoMaker A finished

---
+++ server/router.js
@@ -0,0 +1,21 @@
+// - REQUIRE -
+const controller = require('./controllers');
+
+// - CONNECTIONS -
+const router = ( app ) => {
+  // login
+  app.get('/login', controller.Account.loginPage);  // GET
+  app.post('/login', controller.Account.login);  // POST
+  // signup
+  app.get('/signup', controller.Account.signupPage);  // GET
+  app.post('/signup', controller.Account.signup);  // POST
+  // logout
+  app.get('/logout', controller.Account.logout);
+  // maker
+  app.get('/maker', controller.Domo.makerPage);
+  // /
+  app.get('/', controller.Account.loginPage);  // DEFAULT
+};
+
+// - EXPORTS -
+module.exports = router;
